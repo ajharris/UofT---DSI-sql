65e713582cad40c28737669d7af1393382a357ca
Andrew Harris <andrew.harris@torontomu.ca>
2025-08-08 12:09:39 -0400

    finished assignment 1

--- 02_activities/assignments/assignment1.sql
+++ 02_activities/assignments/assignment1.sql
@@ -89,22 +89,38 @@ SELECT *
 FROM vendor as v
 INNER JOIN vendor_booth_assignments as vba
 	ON v.vendor_id = vba.vendor_id
-	ORDER BY vendor_name, market_date
+	ORDER BY vendor_name, market_date;
 
 /* SECTION 3 */
 
 -- AGGREGATE
 /* 1. Write a query that determines how many times each vendor has rented a booth 
 at the farmer’s market by counting the vendor booth assignments per vendor_id. */
-
-
+SELECT vendor_name,
+COUNT(booth_number) AS booth_count
+FROM vendor as v
+INNER JOIN vendor_booth_assignments vb
+	on v.vendor_id = vb.vendor_id
+	GROUP BY vendor_name
+	ORDER BY vendor_name;
+	
 
 /* 2. The Farmer’s Market Customer Appreciation Committee wants to give a bumper 
 sticker to everyone who has ever spent more than $2000 at the market. Write a query that generates a list 
 of customers for them to give stickers to, sorted by last name, then first name. 
 
 HINT: This query requires you to join two tables, use an aggregate function, and use the HAVING keyword. */
+SELECT customer_last_name,
+customer_first_name,
+SUM(ROUND(cost_to_customer_per_qty * quantity, 2)) AS total_spent
 
+
+FROM customer AS c
+INNER JOIN customer_purchases AS cp
+	ON c.customer_id = cp.customer_id
+	GROUP BY c.customer_id
+	HAVING total_spent > 2000
+ORDER BY customer_last_name, customer_first_name;
 
 
 --Temp Table
@@ -118,6 +134,16 @@ When inserting the new vendor, you need to appropriately align the columns to be
 -> To insert the new row use VALUES, specifying the value you want for each column:
 VALUES(col1,col2,col3,col4,col5) 
 */
+
+DROP TABLE IF EXISTS temp.new_vendor;
+
+CREATE TABLE temp.new_vendor AS
+SELECT * FROM vendor;
+
+INSERT INTO temp.new_vendor (vendor_id, vendor_name, vendor_type, vendor_owner_first_name, vendor_owner_last_name)
+SELECT
+	MAX(vendor_id + 1), 'Thomass Superfood Store', 'Fresh Focused', 'Thomas', 'Rosenthal'
+FROM temp.new_vendor
 
 
 
